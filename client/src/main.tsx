7e0841c043ea2271861619e09d3bf58809aca83d
Yuki Yuigishi <me@yuigishi.dev>
2024-02-12 00:44:18 +0900

    chore:create login form

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,22 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Top Page</div>,
+    },
+    {
+        path: "/login",
+        element: <App />,
+    },
+])
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 )
